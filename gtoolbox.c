b4a780c4991804a7f2c01ab5961c4a474f560911
Toomz <eleonore.pirri@gmail.com>
2012-11-27 11:01:41 +0100

    Création d'une fonction qui crée la matrice

--- gtoolbox.c
+++ gtoolbox.c
@@ -1,36 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <matrice.h>
 
-// test 
-
-int main(int argc, char* argv[]){
+int** create_mat(int nb_som) {
 	
-	int nb_sommets ;
-	printf("Voyez entrer les nombre de sommets souhaités : \n");
-	scanf("%d", &nb_sommets);
-	
-	int matrice[nb_sommets][nb_sommets] ;
+	int ** mat = malloc( nb_som*sizeof(int*) );
 	int i, j ;
 	
-	for(i=0; i<nb_sommets; i++){
-		for(j=0; j<nb_sommets; j++){
-			matrice[i][j] = 0 ;
+	// Initialisation de la matrice à 0
+	for(i=0; i<nb_som; i++){
+		mat[i] = malloc( nb_som*sizeof(int*) );
+		for(j=0; j<nb_som; j++){
+			mat[i][j] = 0 ;
 		}
 	}
 	
-	for(i=0; i<nb_sommets; i++){
+	// Remplissage de la matrice 
+	for(i=0; i<nb_som; i++){
 		
 		int tmp, som;
 		printf("Nombre de sommets reliés au sommet %d \n", i+1);
 		scanf("%d", &tmp);
 		
 		for (j=0; j<tmp; j++){
-			printf("Sommet relié à %d \n", i);
+			printf("Sommet relié à %d \n", i+1);
 			scanf("%d", &som);
-			matrice[i][som] = 1;
+			mat[i][som] = 1;
 		}
 		
-	}
+	}	
+	return mat;
+}
+
+
+int main(int argc, char* argv[]){
+	
+	int nb_sommets, i, j ;
+	printf("Voyez entrer les nombre de sommets souhaités : \n");
+	scanf("%d", &nb_sommets);
+	
+	int **matrice = NULL ;
+	
+	matrice = create_mat(nb_sommets);
+	
 	
 	for(i=0; i<nb_sommets; i++){
 		for(j=0; j<nb_sommets; j++){
